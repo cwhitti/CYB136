92d2b89a258bd8cc84da0caf39f747ee1e9f2639
cwhitti <112997645+cwhitti@users.noreply.github.com>
2024-04-28 16:34:13 -0700

    good enough

--- Assignment_4/NoBugs/main.cpp
+++ Assignment_4/NoBugs/main.cpp
@@ -10,12 +10,24 @@ int main(int argc, char const *argv[])
 {
   // Declare variables
   bool run = true;
-  string input, gradeStr, lastName, firstName, assertMsg;
+  string input, gradeStr, lastName, firstName;
+
+  /*
+    1) Take your code for assignment 2 or 3, and move the functionality
+    into a single function (which may call other functions, of course,
+    if you wrote multiple functions in assignment 2 or 3).
+    Change the user input into a parameter to the function, so the code
+    is now self-contained. You may have to do some serious changes to
+    the code to get this to work, in which case you can move somewhat
+    further from the original submission.
+  */
   string fileName = "student_data.csv" ;
   string lNameFile = "last_names.csv";
   string fNameFile = "first_names.csv";
   ifstream file;
   double grade;
+
+  int index = 0;
 
   // intitialize BSTRoot
   BST studentBST;
@@ -62,6 +74,24 @@ int main(int argc, char const *argv[])
       lastName  = randomName( lastNameVec );
       firstName = randomName( firstNameVec );
       grade     = generateGrade( allGrades );
+
+      // 5)  Write a simple set of tests, including some random generation
+      // of input values, to test your function representing assignment
+      // 2/3's code. Add some tests that detect the contract violation.
+      for ( index = 0; index <= 1000; index ++)
+      {
+        grade     = generateGrade( allGrades );
+
+        // check if grade is bad
+          // grade must be between 0 and 100
+        if ( grade < 0 || grade > 100 )
+        {
+          printString("BAD!!!!!!!!!!!!!!");
+
+          // (6) Write assertions in your code to check a contract
+          assert(("Grade has been generated wrong.", false));
+        }
+      }
 
       // concatenate
       gradeStr = to_string( grade );
@@ -151,6 +181,11 @@ void processFileIntoBST(ifstream& file, BST& studentBST,
     grade = stof(gradeStr);
 
     // check if not a duplicate grade
+    // (6) Add a preconditon to reject the bad inputs;
+    // not just those violating the contract, but the inputs that cause
+    // the non-contract bug to trigger.
+
+
     if ( !gradeInVector( grade, allGrades) )
     {
       // append to vec
@@ -159,10 +194,8 @@ void processFileIntoBST(ifstream& file, BST& studentBST,
       // insert to BST
       studentBST.insert( lastName, firstName, grade );
 
+      // log student to file
       Logger.LogStudent( firstName, lastName, gradeStr );
     }
-
-    // else, skip it
-
   }
 }
